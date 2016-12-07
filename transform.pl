8667597566fe8c96ba4fa351c1c7430fdb02c423
Curtis Schlak <realistschuckle@gmail.com>
2016-12-07 11:53:05 -0500

    Transform CSV to EDI-like thing.
    A solution to the "transform CSV into EDI" laboratory.

---
+++ transform.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+
+open(my $input, '<input.dat')
+    or die('Could not open input.dat.');
+
+open(my $output, '>output.dat')
+    or die('Could not open output.dat.');
+
+<$input>; # discard the header line
+
+print($output "01234***ISA****\n");
+while (my $line = <$input>) {
+    my ($name, $level, $time, $age, $isAwesome) = split(/,/, $line);
+    my $record = <<"EOF";
+GP*01
+N1*$name
+N2*$level
+N3*$time*$age*$isAwesome
+EOF
+    chomp($record);
+    print($output $record);
+}
+
+close($output);
+close($input);
