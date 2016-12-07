80ee880d9aebe56390bc4b128ff599a0e78f030f
Curtis Schlak <realistschuckle@gmail.com>
2016-12-07 13:42:41 -0500

    Change some variable names to match field headers.

--- transform.pl
+++ transform.pl
@@ -2,25 +2,25 @@
 use warnings;
 use strict;
 
-open(my $input, '<input.dat')
-    or die('Could not open input.dat.');
+open(my $input, '<', 'input.dat')
+  or die('Could not open input.dat.');
 
-open(my $output, '>output.dat')
-    or die('Could not open output.dat.');
+open(my $output, '>', 'output.dat')
+  or die('Could not open output.dat.');
 
 <$input>; # discard the header line
 
 print($output "01234***ISA****\n");
 while (my $line = <$input>) {
-    my ($name, $level, $time, $age, $isAwesome) = split(/,/, $line);
-    my $record = <<"EOF";
+  my ($name, $level, $payment, $location_code, $in_network) = split(/,/, $line);
+  my $record = <<"EOF";
 GP*01
 N1*$name
 N2*$level
-N3*$time*$age*$isAwesome
+N3*$payment*$location_code*$in_network
 EOF
-    chomp($record);
-    print($output $record);
+  chomp($record);
+  print($output $record);
 }
 
 close($output);
